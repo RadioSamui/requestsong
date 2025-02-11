a60238e9ef31b2e1a2913670e2fad1cb7afd24fb
Radio Samui <svetskyv@gmail.com>
2025-02-11 10:44:05 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// RadioBOSS API connection details
+$rb_server = 'IP'; // RadioBOSS hostname or IP
+$rb_port = 'PORT API'; // RadioBOSS port
+$rb_password = 'PASS'; // API password
+
+// Плейлист для запроса песен
+$rb_library = 'LIBRARY'; // LIBRARY
+// API URL base
+$rb_api = "http://$rb_server:$rb_port/?pass=$rb_password";
+?>
